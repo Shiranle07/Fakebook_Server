b5db6fa15fe4d04828afc471f2db07a5da0283e1
Your Name <you@example.com>
2024-03-07 14:22:52 +0200

    post options

--- controllers/post.js
+++ controllers/post.js
@@ -4,4 +4,42 @@ const addPost= async(req, res) => {
     res.json(await postService.addPost(req.body.postBody, req.body.postPhoto));
 };
 
-module.exports = {addPost};
+const getPosts= async(_, res) => {
+    res.json(await postService.getPosts());
+};
+
+const getPostById= async(req, res) => {
+    const post = await postService.getPostById(req.params.id);
+    if (!post){
+        return res.status(404).json({errors: {'Post not found'}});
+    }
+    res.json(post);
+};
+
+const editPost= async(req, res) => {
+    try{
+        const post = await postService.editPost(req.params.id, req.body.postBody);
+        if (!post){
+            return res.status(404).json({errors: {'Post not found'}});
+        }
+        res.json(post);
+    }
+    catch{
+        return res.status(404).json({errors: {'Post not found'}});
+    } 
+};
+
+const deletePost= async(req, res) => {
+    try{
+        const post = await postService.deletePost(req.params.id, req.body.postBody);
+        if (!post){
+            return res.status(404).json({errors: {'Post not found'}});
+        }
+        res.json(post);
+    }
+    catch{
+        return res.status(404).json({errors: {'Post not found'}});
+    } 
+};
+
+module.exports = {addPost, getPosts, getPostById, editPost, deletePost};
--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,13 @@ const express = require('express');
 var router = express.Router();
 
 router.route('/')
+    .get(postController.getPosts)
     .post(postController.addPost);
+
+router.route('/:id')
+   .get(postController.getPostById)
+   .patch(postController.editPost)
+   .delete(postController.deletePost)
+
 
 module.exports = router;
